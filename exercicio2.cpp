6cb7cc78e774b91bfa63ea8c0db978aebc80c08b
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-10-13 20:40:17 -0300

    sobrecarga no operador - e !=

--- exercicio2.cpp
+++ exercicio2.cpp
@@ -37,20 +37,77 @@ public:
         }
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ItemSet operator-(const ItemSet& other) const {
+        ItemSet result = *this;
+        for (const string& item : other.conjuntos) {
+            auto it = find(result.conjuntos.begin(), result.conjuntos.end(), item);
+            if (it != result.conjuntos.end()) {
+                result.conjuntos.erase(it);
+            }
+        }
+        return result;
+    }
+   
+    ItemSet operator!=(const ItemSet& other) const {
+        ItemSet result = *this - other; 
+        ItemSet temp = other - *this;   
+        result.conjuntos.insert(result.conjuntos.end(), temp.conjuntos.begin(), temp.conjuntos.end());
+        return result;
+    }
+
 };
 
 int main() {
     ItemSet conjuntoA;
     conjuntoA.inserirItens("Gol1");
     conjuntoA.inserirItens("Gol2");
+    conjuntoA.inserirItens("Gol2");
 
     conjuntoA.mostrarItens();
 
     ItemSet conjuntoB;
-    conjuntoB.inserirItens("Bola1");
+    conjuntoB.inserirItens("Gol1");
     conjuntoB.inserirItens("Bola2");
 
     conjuntoB.mostrarItens();
+
+    ItemSet conjuntoC;
+    conjuntoC.inserirItens("Bola1");
+    conjuntoC.inserirItens("Bola2");
+
+    conjuntoC.mostrarItens();
+
+    ItemSet resultado = conjuntoB - conjuntoA; 
+
+
+
+    cout << "Subtração de (B - A)" << endl;
+    resultado.mostrarItens();
+
+    ItemSet resultado2 = conjuntoB != conjuntoA;
+
+    cout << "Diferença de (B <> A)" << endl;
+
+    resultado2.mostrarItens();
 
 
     return 0;
